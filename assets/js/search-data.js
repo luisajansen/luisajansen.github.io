b0f9df9b2dd6a5dd13eaa5918e5c65857dcc3594
luisajansen <114135163+luisajansen@users.noreply.github.com>
2026-04-08 14:37:22 +0000

    Deploying to gh-pages from @ luisajansen/luisajansen.github.io@3a6bfb7796f0bffa7774e625cce3e4372e07a68a 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -3,8 +3,8 @@ const ninja = document.querySelector('ninja-keys');
 
 // add the home and posts menu items
 ninja.data = [{
-    id: "nav-about",
-    title: "about",
+    id: "nav-luisa-jansen-privacy-data-protection-in-research",
+    title: "Luisa Jansen — Privacy & Data Protection in Research",
     section: "Navigation",
     handler: () => {
       window.location.href = "/";
